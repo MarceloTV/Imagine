f53c7f664ad09b948b83372a02fc139822de72ac
Marcelino A. Abreu Mejia <marcelinomejia2009@gmail.com>
2021-10-16 15:48:56 -0400

    fixes

--- src/components/Body/App/Program/index.jsx
+++ src/components/Body/App/Program/index.jsx
@@ -14,6 +14,12 @@ import ProgramContext from "./context";
 function Program(props){
     
     return <div className="program_container">
+        <div className="alert_pc">
+            <div className="content">
+                <h2>Alert</h2>
+                <p>This app is only in pc</p>
+            </div>
+        </div>
         <ProgramContext>
             <OptionsContainer />
             <Canvas />
